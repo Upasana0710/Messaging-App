99713ed87a10b03eab4073e1236fc55f14c39c2e
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-28 14:35:29 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+import { darkTheme, lightTheme } from "./utils/Themes";
+import styled from 'styled-components';
+import './App.css';
+import Sidebar from "./components/Sidebar";
+import HorizontalBar from "./components/HorizontalBar";
+
+const MainContainer = styled.div`
+  background: ${({ theme }) => theme.bgLight};
+  width: 100%;
+  height: 100vh;
+`;
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <MainContainer>
+        <HorizontalBar />
+        <Sidebar />
+      </MainContainer>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/HorizontalBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HorizontalBar = () => {
+  return (
+    <div>HorizontalBar</div>
+  )
+}
+
+export default HorizontalBar
