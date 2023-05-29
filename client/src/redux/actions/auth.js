5f4c78e66178a0ba8aa94b58f7d04942438a0598
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-29 19:01:28 +0530

    code formatting

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -4,7 +4,6 @@ import * as api from "../../api/index";
 export const signupGoogle = (accessToken, navigate) => async (dispatch)=>{
     try{
         // signup user
-        console.log("running");
         const {data} = await api.signup(accessToken)
 
         dispatch({type : AUTH, data})
@@ -17,10 +16,7 @@ export const signupGoogle = (accessToken, navigate) => async (dispatch)=>{
 export const signinGoogle = (accessToken, navigate) => async (dispatch)=>{
     try{
         // login user
-        console.log("data");
         const {data} = await api.signin(accessToken)
-
-        
         dispatch({type : AUTH, data})
         navigate("/messaging")
     }catch(err){
