2c26bf25d1a688ec8a3fde094a1bb9c0af58fc7e
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-30 02:33:48 +0530

    removed interceptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,12 +2,16 @@ import axios from "axios"
 
 const API = axios.create({baseURL:"http://localhost:5000"})
 
-API.interceptors.request.use((req)=>{
-    if(localStorage.getItem("user_info")){
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user_info").token)}`
-    }
-    return req;
-})
-
+//auth api
 export const signup = (accessToken) => API.post("/user/signup", {googleAccessToken: accessToken})
 export const signin = (accessToken) => API.post("/user/signin", {googleAccessToken: accessToken})
+
+//user api
+export const fetchUser = (id) => API.get(`/user/${id}`);
+
+//Conversation api
+export const getDM=(id)=>API.get(`/conversation/${id}`);
+
+//message api
+export const createMessage=(message)=>API.post('/message',message);
+export const getMessages=(id)=>API.get(`/message/${id}`);
