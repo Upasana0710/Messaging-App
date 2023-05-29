3f4bc0bb0fc7b365b51b0ef38afb014d3219314c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-30 02:36:03 +0530

    added more routes to user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {signup, signin} from '../controllers/userControllers.js';
+import {signup, signin, getUser} from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/signin',signin);
+router.get('/:id',getUser);
 
 export default router;
