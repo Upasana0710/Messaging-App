3cfa66a9e137fd23f3efe17436dbca0c1246196d
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-30 23:06:50 +0530

    added search api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {signup, signin, getUser} from '../controllers/userControllers.js';
+import {signup, signin, getUser, searchUser} from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/signin',signin);
+router.get('/search',searchUser);
 router.get('/:id',getUser);
 
 export default router;
