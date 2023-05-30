577ff82e3491ac70114d1567ef820acf804a8671
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-31 00:46:25 +0530

    changed cors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import userRoutes from '../server/routes/userRoutes.js';
 import messageRoutes from '../server/routes/messageRoutes.js';
 import conversationRoutes from '../server/routes/conversationRoutes.js';
@@ -19,14 +20,14 @@ mongoose.connect(process.env.CONNECTION_URL)
     .catch((err)=>console.log(err));
 
 const corsConfig={
-    credentials: "true",
-    origin: "https://chatzbee.netlify.app",
-    optionSuccessStatus: "200",
+    credentials: true,
+    origin: true,
 };
 
 app.use(helmet());
 app.use(compression());
 app.use(cors(corsConfig));
+app.use(morgan('tiny'));
     
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
